15d9bf6e758bce712fbe7e2ca57ba5bb2bf3411d
JLemmen89 <jlemmen89@protonmail.com>
2021-03-27 19:09:58 -0400

    Beta - Damage Calculator

--- src/classes/CalcDamage.java
+++ src/classes/CalcDamage.java
@@ -1,4 +1,49 @@
 package classes;
 
 public class CalcDamage {
+    private int basicDamage;
+    private int skillDamage;
+    private double skillDamagePer;
+    private int skillEnhancement;
+    private int criticalDamage;
+    private double criticalDamagePer;
+    private double penetration;
+    private double additionalDamagePer;
+    private boolean critical;
+
+    public CalcDamage() {
+        basicDamage = -1;
+        skillDamage = -1;
+        skillDamagePer = -1;
+        skillEnhancement = -1;
+        criticalDamage = -1;
+        criticalDamagePer = -1;
+        penetration = -1;
+        additionalDamagePer = -1;
+        critical = false;
+    }
+    public void setStats(int bDmg, int sDmg, double sDmgP, int sEnh, int cDmg, double cDmgP, double pen, double aDmgP, boolean crit) {
+        basicDamage = bDmg;
+        skillDamage = sDmg;
+        skillDamagePer = sDmgP;
+        skillEnhancement = sEnh;
+        criticalDamage = cDmg;
+        criticalDamagePer = cDmgP;
+        penetration = pen;
+        additionalDamagePer = aDmgP;
+        critical = crit;
+    }
+    public double getDamage(){
+        double totalDamage = 0;
+        if(critical) {
+            totalDamage = (((((basicDamage * 2) + (skillDamage * 2) + (skillDamage * skillDamagePer)
+            + (skillEnhancement * 2) + criticalDamage) * criticalDamagePer) * penetration) * additionalDamagePer);
+        }
+        if(!(critical)){
+            totalDamage = (((basicDamage + skillDamage + (skillDamage * skillDamagePer)
+                    + skillEnhancement) * penetration) * additionalDamagePer);
+        }
+
+        return totalDamage;
+    }
 }
--- src/main/DamageCalculator.java
+++ src/main/DamageCalculator.java
@@ -1,14 +1,23 @@
 package main;
 
+import classes.CalcDamage;
 import java.util.Scanner;
 
 public class DamageCalculator {
 
     public static void main(String[] args){
         Scanner scnr = new Scanner(System.in);
+        CalcDamage calc = new CalcDamage();
 
-            int charLevel = scnr.nextInt();
+        System.out.println("Please enter your character stats in the following order\n" +
+                "Please use a single space between each stat.\n" +
+                "Basic Damage, Skill Damage, Skill Damage %, Skill Enhancement\n" +
+                ", Critical Damage, Critical Damage %, Penetration, Additional Damage %, isCrit.\n" +
+                "Press Enter when all stats have been typed.");
 
-            System.out.println(charLevel);
+        calc.setStats(scnr.nextInt(), scnr.nextInt(), scnr.nextDouble(), scnr.nextInt(), scnr.nextInt(), scnr.nextDouble(),
+                scnr.nextDouble(), scnr.nextDouble(), scnr.hasNextBoolean());
+
+        System.out.print(calc.getDamage());
     }
 }
