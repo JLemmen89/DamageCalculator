a582f1f67f2525cf872cf8e2efd2542955515246
JLemmen89 <jlemmen89@protonmail.com>
2021-03-27 16:00:01 -0400

    Initial Commit

---
+++ src/main/DamageCalculator.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Scanner;
+
+public class DamageCalculator {
+
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+
+            int charLevel = scnr.nextInt();
+
+            System.out.println(charLevel);
+    }
+}
